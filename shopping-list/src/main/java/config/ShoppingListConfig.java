4b44373d16d64f97d76472c4cad399ef5230e842
jochen <jochen.francois@kbc.be>
2019-10-30 09:36:38 +0100

    add mutual TLS

--- shopping-list/src/main/java/config/ShoppingListConfig.java
+++ shopping-list/src/main/java/config/ShoppingListConfig.java
@@ -2,12 +2,16 @@ package config;
 
 import com.ezgroceries.shoppinglist.persistence.repository.CocktailRepository;
 import com.ezgroceries.shoppinglist.persistence.repository.ShoppingListRepository;
+import com.ezgroceries.shoppinglist.service.external.CocktailDBClient;
 import com.ezgroceries.shoppinglist.service.internal.CocktailService;
 import com.ezgroceries.shoppinglist.service.internal.CocktailServiceImpl;
 import com.ezgroceries.shoppinglist.service.internal.ShoppingListService;
 import com.ezgroceries.shoppinglist.service.internal.ShoppingListServiceImpl;
-import com.ezgroceries.shoppinglist.service.external.CocktailDBClient;
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,5 +37,16 @@ public class ShoppingListConfig {
 	@Bean
 	public ShoppingListService shoppingListService(){
 		return new ShoppingListServiceImpl(shoppingListRepository, cocktailRepository);
+	}
+
+	//Custom tomcat configuration, we add an additional connector that allows http traffic next to https
+	@Bean
+	public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setPort(httpPort);
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(connector);
+		return tomcat;
 	}
 }
